d42f2618360341421b14090935d5087d08c94ecf
Zeus6768 <akaps6768@gmail.com>
2022-11-28 21:54:48 +0900

    Update round function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi.responses import FileResponse
 from fastapi import FastAPI
+from myfunc import mRound
 
 app = FastAPI()
 
---
+++ myfunc.py
@@ -0,0 +1,5 @@
+def mRound(n):
+	if (n//1.25)%2 == 0:
+		return n//1.25*1.25
+	else:
+		return (n//1.25+1)*1.25
